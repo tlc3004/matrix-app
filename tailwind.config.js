206befc6e25207bdfa675b73f812b473c51a93f4
tlc3004 <leonantoniobm@gmail.com>
2025-07-31 13:43:16 -0500

    comit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
+export default {
+content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flicker: {
+          '0%, 19%, 21%, 23%, 25%, 54%, 56%, 100%': {
+            opacity: '1',
+            filter: 'drop-shadow(0 0 2px #fff) drop-shadow(0 0 4px #fff) drop-shadow(0 0 8px #0ff)',
+          },
+          '20%, 22%, 24%, 55%': {
+            opacity: '0.4',
+            filter: 'none',
+          },
+        },
+      },
+      animation: {
+        flicker: 'flicker 1.5s infinite',
+      },
+    },
   },
   plugins: [],
 }
